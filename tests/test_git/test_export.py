0abcac160b74a75fcca9edab8774492bfe15cbbc
Robpol86 <robpol86@gmail.com>
2016-06-19 23:42:06 -0700

    test_new_branch_tags().
    Making sure export() works with branches fetched not pulled.

--- tests/test_git/test_export.py
+++ tests/test_git/test_export.py
@@ -4,7 +4,7 @@ import os
 
 import pytest
 
-from sphinxcontrib.versioning.git import export
+from sphinxcontrib.versioning.git import export, fetch_commits, list_remote
 
 
 def test_simple(tmpdir, local, run):
@@ -55,3 +55,51 @@ def test_docs_dir(tmpdir, local, run, levels):
     paths = sorted(f.relto(target) for f in target.visit())
     expected.sort()
     assert paths == expected
+
+
+@pytest.mark.usefixtures('local')
+@pytest.mark.parametrize('mode', ['pushed_tag_of_unpushed_branch', 'annotated_tag_orphaned_branch'])
+def test_new_branch_tags(tmpdir, remote, run, mode):
+    """Test with new branches and tags unknown to local repo.
+
+    :param tmpdir: pytest fixture.
+    :param remote: conftest fixture.
+    :param run: conftest fixture.
+    :param str mode: Test scenario.
+    """
+    # Setup other behind local with just one cloned branch.
+    local = tmpdir.ensure_dir('local2')
+    run(local, ['git', 'clone', '--depth=1', '--branch=feature', remote, '.'])
+    run(local, ['git', 'checkout', '-qf', run(local, ['git', 'rev-parse', 'HEAD']).strip()])
+
+    # Commit to separate local repo and push to common remote.
+    local_ahead = tmpdir.ensure_dir('local_ahead')
+    run(local_ahead, ['git', 'clone', remote, '.'])
+    if mode == 'pushed_tag_of_unpushed_branch':
+        run(local_ahead, ['git', 'checkout', '-b', 'un_pushed_branch'])
+        local_ahead.join('README').write('one')
+        run(local_ahead, ['git', 'commit', '-am', 'Changed new branch'])
+        sha = run(local_ahead, ['git', 'rev-parse', 'HEAD']).strip()
+        run(local_ahead, ['git', 'tag', 'nb_tag'])
+        run(local_ahead, ['git', 'push', 'origin', 'nb_tag'])
+    else:
+        run(local_ahead, ['git', 'checkout', '--orphan', 'orphaned_branch'])
+        local_ahead.join('README').write('two')
+        run(local_ahead, ['git', 'add', 'README'])
+        run(local_ahead, ['git', 'commit', '-m', 'Added new README'])
+        sha = run(local_ahead, ['git', 'rev-parse', 'HEAD']).strip()
+        run(local_ahead, ['git', 'tag', '--annotate', '-m', 'Tag annotation.', 'ob_at'])
+        run(local_ahead, ['git', 'push', 'origin', 'orphaned_branch', 'ob_at'])
+
+    # Fetch.
+    remotes = [r for r in list_remote(str(local)) if r[0] == sha]
+    assert remotes
+    fetch_commits(str(local), remotes)
+
+    # Export.
+    target = tmpdir.ensure_dir('exported', sha)
+    export(str(local), '.', sha, str(target))
+    files = [f.relto(target) for f in target.listdir()]
+    assert files == ['README']
+    expected = 'one' if mode == 'pushed_tag_of_unpushed_branch' else 'two'
+    assert target.join('README').read() == expected
