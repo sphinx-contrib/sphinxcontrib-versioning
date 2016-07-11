b69361950569e5b50dc1e16105d918a213587172
Robpol86 <robpol86@gmail.com>
2016-07-11 00:34:28 -0700

    Started testing main function.
    Only testing with valid docs for now.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from sphinxcontrib.versioning.__main__ import __doc__ as doc, get_arguments
+from sphinxcontrib.versioning.__main__ import __doc__ as doc, get_arguments, main
 
 ARGV = (__file__, 'build', 'docs', 'html')
 
@@ -37,3 +37,47 @@ def test_get_arguments_string(mode):
     config = get_arguments(argv, doc)
     assert config['--root-ref'] == expected['--root-ref']
     assert config['--additional-src'] == expected['--additional-src']
+
+
+def test_main_working(monkeypatch, tmpdir, local_docs, run):
+    """Test main() with working docs.
+
+    :param monkeypatch: pytest fixture.
+    :param tmpdir: pytest fixture.
+    :param local_docs: conftest fixture.
+    :param run: conftest fixture.
+    """
+    monkeypatch.chdir(local_docs)
+    local_docs.ensure('subdir', 'sub.rst').write(
+        '.. _sub:\n'
+        '\n'
+        'Sub\n'
+        '===\n'
+        '\n'
+        'Sub directory sub page documentation.\n'
+    )
+    local_docs.join('contents.rst').write('    subdir/sub\n', mode='a')
+    run(local_docs, ['git', 'add', 'subdir', 'contents.rst'])
+    run(local_docs, ['git', 'commit', '-m', 'Adding subdir docs.'])
+    run(local_docs, ['git', 'tag', 'v1.0.0'])
+    run(local_docs, ['git', 'push', 'origin', 'master', 'v1.0.0'])
+
+    destination = tmpdir.ensure_dir('destination')
+    config = get_arguments([__file__, 'build', '.', str(destination)], doc)
+    main(config)
+
+    # Check master.
+    contents = destination.join('contents.html').read()
+    assert '<li><a href="contents.html">master</a></li>' in contents
+    assert '<li><a href="v1.0.0/contents.html">v1.0.0</a></li>' in contents
+    contents = destination.join('subdir', 'sub.html').read()
+    assert '<li><a href="../contents.html">master</a></li>' in contents
+    assert '<li><a href="../v1.0.0/contents.html">v1.0.0</a></li>' in contents
+
+    # Check v1.0.0.
+    contents = destination.join('v1.0.0', 'contents.html').read()
+    assert '<li><a href="../contents.html">master</a></li>' in contents
+    assert '<li><a href="../v1.0.0/contents.html">v1.0.0</a></li>' in contents
+    contents = destination.join('v1.0.0', 'subdir', 'sub.html').read()
+    assert '<li><a href="../../contents.html">master</a></li>' in contents
+    assert '<li><a href="../../v1.0.0/contents.html">v1.0.0</a></li>' in contents
